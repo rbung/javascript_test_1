4024635cbf7aa1d01bd3304defe5641725984a57
CARE-COLIN Thibaut <carecolin@gmail.com>
2018-02-23 12:27:00 +0100

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,23 @@
 
+'use strict'
+
+module.exports =  {
+  testStrings: [
+    'Hello world',
+    'Testing with javascript is funny',
+    'I write a palindrome radar',
+    'Milk, butter, bread, orange juice, pancakes..',
+    'No x in Nixon',
+    'I love kayak',
+    'I wish i could drive a tesla or a racecar',
+    'Was it a car or a cat I saw',
+    'The answer to this test is 42',
+    'obi wan kenobi',
+    'Kayak ',
+    'R A d D a R ',
+    'Murder for a jar of red rum.',
+    'this drone is not a palyndrome hit',
+    'A nut for a jar of tuna.',
+    'King, are you glad you are king?'
+  ]
+};
